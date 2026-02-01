dd7c1b3502644fe3d956e17e8103df3f62a5d07c
simon1g <szynka305@gmail.com>
2026-02-01 15:04:06 +0100

    feat: add About component to display personal interests and social media links.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Telescope, Code, Palette, Gamepad2, Mountain, Bike, Star, Tv, Twitter, Youtube, Music, PawPrint } from 'lucide-react';
+import { Telescope, Code, Palette, Gamepad2, Mountain, Bike, Star, Tv, Twitter, Youtube, Music, PawPrint, Cloud } from 'lucide-react';
 import '../styles/interests.css';
 import '../styles/interests.css';
 
@@ -14,12 +14,6 @@ const NGLIcon = ({ size = 24 }) => (
 const SteamIcon = ({ size = 24 }) => (
     <svg width={size} height={size} viewBox="0 0 32 32" fill="currentColor">
         <path d="M18.102 12.129c0-0 0-0 0-0.001 0-1.564 1.268-2.831 2.831-2.831s2.831 1.268 2.831 2.831c0 1.564-1.267 2.831-2.831 2.831-0 0-0 0-0.001 0h0c-0 0-0 0-0.001 0-1.563 0-2.83-1.267-2.83-2.83 0-0 0-0 0-0.001v0zM24.691 12.135c0-2.081-1.687-3.768-3.768-3.768s-3.768 1.687-3.768 3.768c0 2.081 1.687 3.768 3.768 3.768v0c2.080-0.003 3.765-1.688 3.768-3.767v-0zM10.427 23.76l-1.841-0.762c0.524 1.078 1.611 1.808 2.868 1.808 1.317 0 2.448-0.801 2.93-1.943l0.008-0.021c0.155-0.362 0.246-0.784 0.246-1.226 0-1.757-1.424-3.181-3.181-3.181-0.405 0-0.792 0.076-1.148 0.213l0.022-0.007 1.903 0.787c0.852 0.364 1.439 1.196 1.439 2.164 0 1.296-1.051 2.347-2.347 2.347-0.324 0-0.632-0.066-0.913-0.184l0.015 0.006zM15.974 1.004c-7.857 0.001-14.301 6.046-14.938 13.738l-0.004 0.054 8.038 3.322c0.668-0.462 1.495-0.737 2.387-0.737 0.001 0 0.002 0 0.002 0h-0c0.079 0 0.156 0.005 0.235 0.008l3.575-5.176v-0.074c0.003-3.12 2.533-5.648 5.653-5.648 3.122 0 5.653 2.531 5.653 5.653s-2.531 5.653-5.653 5.653h-0.131l-5.094 3.638c0 0.065 0.005 0.131 0.005 0.199 0 0.001 0 0.002 0 0.003 0 2.342-1.899 4.241-4.241 4.241-2.047 0-3.756-1.451-4.153-3.38l-0.005-0.027-5.755-2.383c1.841 6.345 7.601 10.905 14.425 10.905 8.281 0 14.994-6.713 14.994-14.994s-6.713-14.994-14.994-14.994c-0 0-0.001 0-0.001 0h0z" />
-    </svg>
-);
-
-const SoundCloudIcon = ({ size = 24 }) => (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
-        <path d="M0 15a1.5 1.5 0 0 1 1.5-1.5v3A1.5 1.5 0 0 1 0 15Zm2.5-.5a1.5 1.5 0 0 1 1.5-1.5v4.5a1.5 1.5 0 0 1-1.5-1.5v-1.5Zm2.5-1a1.5 1.5 0 0 1 1.5-1.5v5.5a1.5 1.5 0 0 1-1.5-1.5v-2.5Zm2.5-1.5a1.5 1.5 0 0 1 1.5-1.5v7.5a1.5 1.5 0 0 1-1.5-1.5v-4.5Zm2.5-3.5a1.5 1.5 0 0 1 1.5-1.5v11.5a1.5 1.5 0 0 1-1.5-1.5V7.5Zm2.5-2.5a1.5 1.5 0 0 1 1.5-1.5v15.5a1.5 1.5 0 0 1-1.5-1.5V5Zm2.5-1.5a1.5 1.5 0 0 1 1.5-1.5v18.5a1.5 1.5 0 0 1-1.5-1.5V3.5Zm2.5 5h1a4 4 0 0 1 4 4v.5a4 4 0 0 1-4 4h-1v-8.5Z" />
     </svg>
 );
 
@@ -112,7 +106,7 @@ export default function About() {
                     <span>NGL</span>
                 </a>
                 <a href="https://soundcloud.com/simon1g-xyz" target="_blank" rel="noopener noreferrer" className="about-social-link soundcloud">
-                    <SoundCloudIcon size={24} />
+                    <Cloud size={24} />
                     <span>SoundCloud</span>
                 </a>
                 <a href="https://www.reddit.com/user/_ssSimon_/" target="_blank" rel="noopener noreferrer" className="about-social-link reddit">
